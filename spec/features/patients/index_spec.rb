c8b2e074c0575f4014a7be283397eff6134b986b
Antonio King <antonio.j.king@gmail.com>
2021-08-09 11:35:20 -0600

    Create patient index page test

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Patient Index Page' do
+  # As a visitor
+  # When I visit the patient index page
+  # I see the names of all patients listed from oldest to youngest
+  it 'displays names of all patients in descending order' do
+    visit "/patients"
+
+    expect(page).to have_content(@patient1.name)
+    expect(page).to have_content(@patient2.name)
+    expect(page).to have_content(@patient3.name)
+    expect(@patient2.name).to appear_before(@patient3.name)
+    expect(@patient3.name).to appear_before(@patient1.name)
+  end
+end
