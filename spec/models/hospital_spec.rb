65f0bfe2621e6b47c8c1245a96fc8eab679153cc
Antonio King <antonio.j.king@gmail.com>
2021-08-09 14:22:16 -0600

    Create uniq_univ test for hospital model

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  describe 'instance methods' do
+    it 'uniq_universities' do
+      doctor4 = @hospital1.doctors.create!(name: 'Adam Lambert', specialty: 'Oncology', university: 'John Hopkins University')
+
+      expect(@hospital1.uniq_universities.length).to eq(2)
+      expect(@hospital1.uniq_universities).to include(@doctor1.university)
+      expect(@hospital1.uniq_universities).to include(@doctor2.university)
+    end
+  end
 end
