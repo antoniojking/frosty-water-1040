98c0e4f5873514e0a5cb9d1d72bcfd9aca6e29ae
Antonio King <antonio.j.king@gmail.com>
2021-08-09 11:43:24 -0600

    Add desc_order method for patient model, fix test

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :treatments
   has_many :doctors, through: :treatments
+
+  def self.desc_order
+    order(age: :desc)
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Patient do
   describe 'class method' do
     it 'desc_order' do
       expect(Patient.desc_order.count).to eq(3)
-      expect(Patient.desc_order.first).to eq(@patient2.name)
-      expect(Patient.desc_order.last).to eq(@patient1.name)
+      expect(Patient.desc_order.first).to eq(@patient2)
+      expect(Patient.desc_order.last).to eq(@patient1)
     end
   end
 end
