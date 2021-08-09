1ebc3c6daecea4d54d2962c8a88a18a6f66851a1
Antonio King <antonio.j.king@gmail.com>
2021-08-09 11:40:07 -0600

    Create class method test for patient display order

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Patient do
     it { should have_many(:treatments) }
     it { should have_many(:doctors).through(:treatments) }
   end
+
+  describe 'class method' do
+    it 'desc_order' do
+      expect(Patient.desc_order.count).to eq(3)
+      expect(Patient.desc_order.first).to eq(@patient2.name)
+      expect(Patient.desc_order.last).to eq(@patient1.name)
+    end
+  end
 end
